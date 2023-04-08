b467096df3f2661b508f1f7af4be51d00b69ea4f
Jeremy Shen <jeremyshen1987@gmail.com>
2023-04-07 18:44:57 -0600

    single event handler for multiple fields

--- src/Register.js
+++ src/Register.js
@@ -8,31 +8,33 @@ const Register = () => {
 
     const [hasRegistered, setHasRegistered] = useState(false)
     const [error, setError] = useState('')
-
+    const [userInfo, setUserInfo] = useState({})
     const overlay = useRef()
+
+    function handleChange(e){
+    
+        console.log('change')
+
+        setUserInfo({
+            ...userInfo,
+            [e.target.name]: e.target.value
+        })
+    }
+
 
     async function register(e){
 
         e.preventDefault()
-        console.log(e.target.parentElement.username)
+        toggle_overlay()
 
-        const pw = e.target.parentElement.password.value
-        const pw2 = e.target.parentElement.password2.value
-
-        if(pw !== pw2){
+        if(userInfo.password !== userInfo.password2){
             alert('Password mismatch')
             return
         }
 
-        toggle_overlay()
-
-        const formElm = e.target.parentElement
         const formData = {
-            username: formElm.username.value,
-            password: await bcrypt.hash(formElm.password.value, 10),
-            firstname: formElm.firstname.value,
-            lastname: formElm.lastname.value,
-            email: formElm.email.value,
+            ...userInfo,
+            password: await bcrypt.hash(userInfo.password, 10)
         }
         
 
@@ -62,58 +64,7 @@ const Register = () => {
         
     }
 
-    const RegForm = () => {
 
-        return(
-
-            <div >
-                <h1 className="registration">Registration</h1>
-                <form className="register_container" action="" method="POST">
-                    
-                    <div className="registration">
-                        <label htmlFor="username">Username:
-                            <input className="registration" name="username" placeholder="Username" type="text" required/>
-                        </label>
-                    </div>
-
-                    <div className="registration">
-                        <label htmlFor="email">Email:
-                        <input className="registration" name="email" placeholder="Email" type="email" required/>
-                        </label>
-                    </div>
-
-                    <div className="registration">
-                        <label htmlFor="firstname">First Name:
-                            <input className="registration" name="firstname" placeholder="First Name" type="text" required/>
-                        </label>
-                    </div>
-
-                    <div className="registration">
-                        <label htmlFor="lastname">Last Name:
-                            <input className="registration" name="lastname" placeholder="Last Name" type="text" required/>
-                        </label>
-                    </div>
-                    
-                    <div className="registration">
-                        <label htmlFor="password">Password:
-                            <input className="registration" name="password" type="password" required/>
-                        </label>
-                    </div>
-
-                    <div className="registration">
-                        <label htmlFor="password">Confirm Password:
-                            <input className="registration" name="password2" type="password" required/>
-                        </label>
-                    </div>
-                    
-                    <button id="signup" onClick={(e) => register(e)}>Sign Up</button>
-                </form>
-                <h3 className='login_link'>Already have an account? &nbsp; &nbsp;<Link to={'/login'}>Login</Link></h3>
-
-            </div>
-
-        )
-    }
 
     function toggle_overlay(){
         
@@ -131,8 +82,59 @@ const Register = () => {
 
         <>  
             <div className="registration_container">
-                {hasRegistered || <RegForm />}
+
+                {hasRegistered ||             
+                
+                <div >
+                    <h1 className="registration">Registration</h1>
+                    <form className="register_container" action="" method="POST">
+                        
+                        <div className="registration">
+                            <label htmlFor="username">Username:
+                                <input onChange={handleChange} className="registration" name="username" placeholder="Username" type="text" required/>
+                            </label>
+                        </div>
+        
+                        <div className="registration">
+                            <label htmlFor="email">Email:
+                            <input onChange={handleChange} className="registration" name="email" placeholder="Email" type="email" required/>
+                            </label>
+                        </div>
+        
+                        <div className="registration">
+                            <label htmlFor="firstname">First Name:
+                                <input onChange={handleChange} className="registration" name="firstname" placeholder="First Name" type="text" required/>
+                            </label>
+                        </div>
+        
+                        <div className="registration">
+                            <label htmlFor="lastname">Last Name:
+                                <input onChange={handleChange} className="registration" name="lastname" placeholder="Last Name" type="text" required/>
+                            </label>
+                        </div>
+                        
+                        <div className="registration">
+                            <label htmlFor="password">Password:
+                                <input onChange={handleChange} className="registration" name="password" type="password" required/>
+                            </label>
+                        </div>
+        
+                        <div className="registration">
+                            <label htmlFor="password">Confirm Password:
+                                <input onChange={handleChange} className="registration" name="password2" type="password" required/>
+                            </label>
+                        </div>
+                        
+                        <button id="signup" onClick={(e) => register(e)}>Sign Up</button>
+                    </form>
+                    <h3 className='login_link'>Already have an account? &nbsp; &nbsp;<Link to={'/login'}>Login</Link></h3>
+    
+                </div>}
+
+
+
                 {error ? <h1 style={{color: "red", textAlign: 'center'}}>{error} already taken</h1> : ''}
+
                 {hasRegistered && <h1>Registered! Please <Link to={'/login'}>Login</Link></h1>}
 
                 <Helper overlay_text = 'Waking up backend... please wait' ref={overlay}/>
@@ -141,5 +143,7 @@ const Register = () => {
 
     )
 }
+
+
 
 export default Register
